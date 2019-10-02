2410aee30a0273c7c13ee1b98f869b6b31e58484
Daniel Clarke <deliotclarke@gmail.com>
2019-10-02 16:34:47 -0500

    removed more.

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -3,28 +3,12 @@ import Header from '../components/Header';
 import Meta from '../components/Meta';
 import styled from 'styled-components';
 
-const MyButton = styled.button`
-  background: red;
-  font-size: ${props => (props.huge ? '100px' : '50px')};
-  span {
-    font-size: 100px;
-  }
-`;
-
 export default class Page extends Component {
   render() {
     return (
       <div>
         <Meta />
         <Header />
-        <MyButton huge>
-          Click
-          <span>💩</span>
-        </MyButton>
-        <MyButton>
-          Click
-          <span>💩</span>
-        </MyButton>
         {this.props.children}
       </div>
     )
