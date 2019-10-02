3a2ccea98e0226a91082cf3c2abf9702e2220817
Daniel Clarke <deliotclarke@gmail.com>
2019-10-02 16:28:48 -0500

    nested selectos are neat.

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 import Meta from '../components/Meta';
+import styled from 'styled-components';
+
+const MyButton = styled.button`
+  background: red;
+  font-size: 50px;
+  span {
+    font-size: 100px;
+  }
+`;
 
 export default class Page extends Component {
   render() {
@@ -8,6 +17,10 @@ export default class Page extends Component {
       <div>
         <Meta />
         <Header />
+        <MyButton>
+          Click
+          <span>💩</span>
+        </MyButton>
         {this.props.children}
       </div>
     )
