7ae28d2994b113e45e6c93c8ba6e3088535664ad
Daniel Clarke <deliotclarke@gmail.com>
2019-10-04 18:21:34 -0500

    added deleteitem component, and imported the button to item component.

---
+++ sick-fits/frontend/components/DeleteItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class DeleteItem extends Component {
+  render() {
+    return (
+      <button>{this.props.children}</button>
+    )
+  }
+}
--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -7,6 +7,7 @@ import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 
 import formatMoney from '../lib/formatMoney';
+import DeleteItem from '../components/DeleteItem'
 
 export default class Item extends Component {
   static propTypes = {
@@ -42,7 +43,7 @@ export default class Item extends Component {
             <a>Edit ✏️</a>
           </Link>
           <button>Add To Cart</button>
-          <button>Delete</button>
+          <DeleteItem>Delete this Item</DeleteItem>
         </div>
       </ItemStyles>
     )
