8d255fa13c15d2fa91a2b6b99161400f8062aa65
Daniel Clarke <deliotclarke@gmail.com>
2019-10-04 17:31:13 -0500

    added mutation and query to corresponding files and updated graphql. backend for update item written and tested

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -9,15 +9,23 @@ const mutations = {
     }, info);
 
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    //first take a copy of the updates
+    const updates = { ...args };
+    //remove the id
+    delete updates.id;
+    //run update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        },
+      },
+      info
+    );
   }
-
-  // createDog(parent, args, ctx, info) {
-  //   global.dogs = global.dogs || [];
-  //   //create a dog
-  //   const newDog = { name: args.name };
-  //   global.dogs.push(newDog);
-  //   return newDog;
-  // }
 };
 
 module.exports = mutations;
