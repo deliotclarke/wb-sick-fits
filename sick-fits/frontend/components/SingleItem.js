7939fb0c7662679890eb409646d936db0368f92a
Daniel Clarke <deliotclarke@gmail.com>
2019-10-05 16:02:43 -0500

    added single item styles. also created SinglePriceTag to work with details page. tried importing the out of box price tag, but it wouldn't work in the context of this page.

--- sick-fits/frontend/components/SingleItem.js
+++ sick-fits/frontend/components/SingleItem.js
@@ -1,9 +1,80 @@
 import React, { Component } from 'react'
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import Error from './ErrorMessage';
+import styled from 'styled-components';
+import Head from 'next/head';
+import PriceTag from './styles/PriceTag';
+import formatMoney from '../lib/formatMoney';
+
+const SingleItemStyles = styled.div`
+  max-width: 1200px;
+  margin: 2rem auto;
+  box-shadow: ${props => props.theme.bs};
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  min-height: 800px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+  .details {
+    margin: 3rem;
+    font-size: 2rem;
+  }
+`;
+
+const SinglePriceTag = styled.span`
+  background: ${props => props.theme.red};
+  transform: rotate(3deg);
+  color: white;
+  font-weight: 600;
+  padding: 5px;
+  line-height: 1;
+  font-size: 3rem;
+  display: inline-block;
+`;
+
+
+const SINGLE_ITEM_QUERY = gql`
+  query SINGLE_ITEM_QUERY($id: ID!) {
+    item(where: {id: $id}) {
+      id
+      title
+      description
+      largeImage
+      price
+    }
+  }
+`;
 
 export default class SingleItem extends Component {
   render() {
     return (
-      <p>Single Item Component</p>
+      <Query query={SINGLE_ITEM_QUERY} variables={{ id: this.props.id }}>
+        {({ error, loading, data }) => {
+          if (error) return <Error error={error} />;
+          if (loading) return <p>Loading...</p>;
+          if (!data.item) return <p>No Item Found for {this.props.id}</p>;
+          const item = data.item;
+          return (
+            <SingleItemStyles>
+              <Head>
+                <title>Sick Fits | {item.title}</title>
+              </Head>
+              <img src={item.largeImage} alt={item.title} />
+              <div className="details">
+                <h2>Viewing {item.title}</h2>
+                <p>{item.description}</p>
+                <SinglePriceTag>{formatMoney(item.price)}</SinglePriceTag>
+              </div>
+            </SingleItemStyles>
+          );
+        }
+        }
+      </Query >
     )
   }
 }
